93ca402384e6ec2b7e72bad9b5f28ada94d618ee
inspirationdev1 <inspirationdev1@gmail.com>
2026-03-08 00:41:21 +0530

    changed baseUrl in fronend

--- src/environment.js
+++ src/environment.js
@@ -1,2 +1,3 @@
-   const baseUrl = 'http://localhost:5001/api';
+   // const baseUrl = 'http://localhost:5001/api';
+   const baseUrl = 'https://school-api-rgq0.onrender.com/api';
    export {baseUrl}

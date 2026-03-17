7ea19952d0b01971055db12e7a83fbe8d407078c
inspirationdev1 <inspirationdev1@gmail.com>
2026-03-18 00:01:41 +0530

    Changed in Environment settings

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,6 @@
-   const baseUrl = 'http://localhost:5001/api';
-   const frontendUrl = 'http://localhost:5173';
-   // const baseUrl = 'https://school-api-rgq0.onrender.com/api';
+   // const baseUrl = 'http://localhost:5001/api';
+   // const frontendUrl = 'http://localhost:5173';
+   const baseUrl = 'https://school-api-rgq0.onrender.com/api';
+   const frontendUrl = 'https://school-frontend-m01u.onrender.com';
+   // 
    export {baseUrl,frontendUrl}

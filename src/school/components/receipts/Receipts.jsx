5de834625e0805e6387e315b04375bd7490df5f4
inspirationdev1 <inspirationdev1@gmail.com>
2026-08-20 15:58:46 +0530

    receipt paidamount validation removed

--- src/school/components/receipts/Receipts.jsx
+++ src/school/components/receipts/Receipts.jsx
@@ -262,12 +262,6 @@ export default function Receipts() {
           break; // exit loop when condition met
         }
 
-        if (item.paidAmount === 0) {
-          setDataError("paidAmount must be greater than 0");
-          hasInvalidRow = true;
-          break; // exit loop when condition met
-        }
-
         console.log(item);
       }
       if (hasInvalidRow) {
